13dd05a0c2702da139b80cda7655fefacb95b3ca
Minhaj Khan <minhajaswe@gmail.com>
2020-09-30 07:39:33 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,17 +1,13 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  if (index >=0 && index < 9) && !position_taken?(board,index)
-    return true
-  if !(position_taken? == true)
-    return true
-  end
+
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
-def position_taken?(board,index)
-  if (board[index] == "X" || board[index] == "O")
+def position_taken?(board, index)
+  if board[index] == "" || board[index] == " "
+    return FALSE
+  else
     return true
-  elsif (board[index] == "" || board[index] == " ")
-    return false
   end
 end
